4a77d0e7cc6755d532c84030fa069e0e4966feff
sborkowski3 <60120757+sborkowski3@users.noreply.github.com>
2020-02-09 21:21:26 -0600

    added information to choice 1 and choice 2

--- MovieListing/BorkowskiAssign3.cs
+++ MovieListing/BorkowskiAssign3.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using NLog;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace MovieData
 {
@@ -26,16 +29,114 @@ namespace MovieData
             var logger = N.LogLogManager.GetCurrentClassLogger();
             logger.Info("Program started");
 
-            //make a menu, ask for input
-            Console.WriteLine("Enter 1 to create data file");
-            Console.WriteLine("Enter 2 to parse data");
-            Console.WriteLine("Enter anything else to quit");
-            //input response
-            string resp = Console.ReadLine();
+            if (!File.Exists(file))
+            {
+                loggerError("File does not exist {File}", file);
+            }
+            else
+            {
+                string choice;
+                {
+                    Console.WriteLine("1) Add your favorite Movie");
+                    Console.WriteLine("2) Show all Movies from list");
+                    Console.WriteLine("3) Press any button to exit");
 
-            //specify path for data file
-            string file = "users/sborkowski3.downloads/movies.csv";
+                    choice = Console.ReadLine();
+                    logger.Info("User choice: {Choice}", choice);
+
+                    List<UInt64> MovieIds = new List<UInt64>();
+                    List<string> MovieTitles = new List<string>();
+                    List<string> MovieGenres = new List<string>();
+
+                    try
+                    {
+                        StreamReader sr = new StreamReader(file);
+                        sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            int idx = line.IndexOf('"');
+                            if (idx == -1)
+                            {
+                                string[] movieDetails = line.Split(',');
+                                MovieIds.Add(UInt64.Parse(movieDetails[0]));
+                                MovieGenres.Add(movieDetails[2].Replace("|", ","));
+                            }
+                            else
+                            {
+                                MovieIds.Add(UInt64.Parse(line.Substring(0, idx - 1)));
+                                line = line.Substring(idx + 1);
+                                idx = line.IndexOf('"');
+                                MovieTitles.Add(line.Substring(0, idx));
+                                line = line.Substring(idx + 2);
+                                MovieGenres.Add(line.Replace("|", ","));
+                            }
+                        }
+                        sr.Close();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex.Message);
+                    }
+                    logger.Info("Movies in file {Count}", MovieIds.Count);
+
+                    if (choice == "1")
+                    {
+                        Console.WriteLine("Enter the movie title: ");
+                        string MovieTitle = Console.ReadLine();
+                        List<string> LowerCaseMovieTitles = MovieTitles.ConvertAll(t => t.ToLower());
+                        if (LowerCaseMovieTitles.Contains(MovieTitle.ToLower()))
+                        {
+                            Console.WriteLine("That movie has already been entered");
+                            logger.Info("Duplicate movie title {Title}", movieTitle);
+                        }
+                        else
+                        {
+                            UInt64 movieId = MovieIds.Max() + 1;
+                            List<string> genres = new List<string>();
+                            string genre;
+                        }
+                        do
+                        {
+                            Console.WriteLine("Enter genre (or done to quit)");
+                            genre = Console.ReadLine();
+                            if (genre != "done" && genre.Length > 0)
+                            {
+                                genres.Add(genre);
+                            }
+                        } while (genre != "done");
+                        if (genre.Count == 0)
+                        {
+                            genres.Add("(no genres listed)");
+                        }
+                        string genreString = string.Join("|", genres);
+                        movieTitle = movieTitle.IndexOf(',') != -1 ? $"\"{MovieTitle}\"" : MovieTitle;
+                        StreamWriter sw = new StreamWriter(file, true);
+                        sw.WriteLine($"{movieId},{movieTitle},{genreString}");
+                        sw.Close();
+                        MovieIds.Add(movieId);
+                        MovieTitles.Add(genreString);
+                        logger.info("Movie id {Id} added", movieId);
+
+                    }
+                }
+                else if (choice == "2")
+                {
+                    for (int i = 0; i < MovieIds.Count; i++)
+                    {
+                        Console.WriteLine($"Id: {MovieIds[i]}");
+                        Console.WriteLine($"Title: {MovieTitles[i]}");
+                        Console.WriteLine($"Genre(s): {MovieGenres[i]}");
+                        Console.WriteLine();
+                    }
+                }
+            
+            while (choice == "1" || choice == "2") ;
 
         }
-    }
+        logger.Info("Program ended");
+       }
 }
+    }
+
