4bcdf5af6153fb23701e9f97821858a289431541
Stephanie Borkowski <sborkowski3@my.wctc.edu>
2020-03-02 21:24:27 -0600

    Created a text file and fixed the code looks to be working

---
+++ MediaLibrary/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Class1
+{
+	public abstract Class1()
+	{
+        
+	}
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public virtual void Talk()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void Talk()
+        {
+            Console.WriteLine("Bark");
+        }
+        public class Cat : Animal
+        {
+            public override Talk()
+            {
+                Console.WriteLine("Meow");
+            }
+        }
+    }
+    public class Bird : Animal
+    {
+        public override Talk()
+        {
+            Console.WriteLine("Cheep");
+        }
+    }
+}
